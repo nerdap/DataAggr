c70d8c059fc9e8be6585be61ecd54717e2adb777
Apoorv Upreti <apoorvupreti@gmail.com>
2022-03-09 17:40:25 +0000

    cleanup comments and fix formatting

--- main.cpp
+++ main.cpp
@@ -1,16 +1,3 @@
-//	Time to chew steel.
-
-// later
-//  if explorer crashes, will the icon disappear
-//  get a proper icon
-// ini loading
-
-//next steps:
-//  remove ghost icon
-// change text, bg color
-
-
-// Include Windows.h before all others.
 #ifdef UNICODE
 #undef UNICODE
 #endif
@@ -65,27 +52,28 @@ HWND hwndEdit = nullptr;
 HMENU hTrayMenu = nullptr;
 NOTIFYICONDATA nid;
 
-int CDECL MessageBoxPrintf (TCHAR * szCaption, TCHAR * szFormat, ...)
+int CDECL MessageBoxPrintf(TCHAR* szCaption, TCHAR* szFormat, ...)
 {
-     TCHAR   szBuffer [1024] ;
-     va_list pArgList ;
+     TCHAR szBuffer[1024];
+     va_list pArgList;
 
-          // The va_start macro (defined in STDARG.H) is usually equivalent to:
-          // pArgList = (char *) &szFormat + sizeof (szFormat) ;
+	// The va_start macro (defined in STDARG.H) is usually equivalent to:
+	// pArgList = (char*)&szFormat + sizeof(szFormat);
+     va_start(pArgList, szFormat);
 
-     va_start (pArgList, szFormat) ;
+    // The last argument to wvsprintf points to the arguments
+     _vsntprintf_s(
+		szBuffer,
+		sizeof(szBuffer) / sizeof(TCHAR), 
+        szFormat,
+		pArgList);
 
-          // The last argument to wvsprintf points to the arguments
+    // The va_end macro just zeroes out pArgList for no good reason
+	va_end(pArgList);
 
-     _vsntprintf_s (szBuffer, sizeof (szBuffer) / sizeof (TCHAR), 
-                  szFormat, pArgList) ;
-
-          // The va_end macro just zeroes out pArgList for no good reason
-
-     va_end (pArgList) ;
-
-     return MessageBox (nullptr, szBuffer, szCaption, 0) ;
+     return MessageBox(nullptr, szBuffer, szCaption, 0);
 }
+
 void debugBox(LPCTSTR debugText)
 {
 	MessageBox(nullptr, debugText, TEXT("Debug Text"), MB_OK);
@@ -98,8 +86,7 @@ void ToggleWindowState();
 void SaveNotes(const std::string&, const std::string&);
 std::string GetTimeStamp();
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int iCmdShow)
-{
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int iCmdShow) {
 	MSG msg;
 	WNDCLASSEX wndclass;
 
@@ -130,29 +117,32 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine,
 	int xWindow = cx - WINDOW_WIDTH;
 	int yWindow = 0;
 
-	hwnd = CreateWindowEx(WS_EX_TOOLWINDOW | WS_EX_TOPMOST | WS_EX_LAYERED,
-						szAppName,
-						TEXT("Data Aggregator (in-dev)"),
-						WS_POPUP,
-						xWindow,
-						yWindow,
-						WINDOW_WIDTH,
-						WINDOW_HEIGHT,
-						nullptr,
-						nullptr,
-						hInstance,
-						nullptr);
+	hwnd = CreateWindowEx(
+		WS_EX_TOOLWINDOW | WS_EX_TOPMOST | WS_EX_LAYERED,
+		szAppName,
+		TEXT("Data Aggregator (in-dev)"),
+		WS_POPUP,
+		xWindow,
+		yWindow,
+		WINDOW_WIDTH,
+		WINDOW_HEIGHT,
+		nullptr,
+		nullptr,
+		hInstance,
+		nullptr);
 	SetLayeredWindowAttributes(hwnd, 0, BYTE(255 * 0.8), LWA_ALPHA);	//	Make window translucent
 	ShowWindow(hwnd, SW_HIDE);	// Window is hidden on startup
 	UpdateWindow(hwnd);
 	RegisterHotKey(hwnd, ID_HOTKEY, hotkey_modifiers, hotkey_char);
 	
-	while(GetMessage(&msg, nullptr, 0, 0))
-	{
-		//	We process VK_ESCAPE before Translating the message so it isn't handled the Edit control instead
-		if(msg.message == WM_KEYDOWN)
-			if(GetAsyncKeyState(VK_ESCAPE))
+	while(GetMessage(&msg, nullptr, 0, 0)) {
+		//	We process VK_ESCAPE before Translating the message so it isn't
+		// handled the Edit control instead
+		if(msg.message == WM_KEYDOWN) {
+			if(GetAsyncKeyState(VK_ESCAPE)) {
 				SendMessage(hwnd, WM_CLOSE, 0, 0);
+			}
+		}
 		TranslateMessage(&msg);
 		DispatchMessage(&msg);
 	}
@@ -160,24 +150,26 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine,
 	return msg.wParam;
 }
 
-LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
-{
+LRESULT CALLBACK WndProc(
+	HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) {
+
 	HDC hdc;
 	PAINTSTRUCT ps;
-	switch(message)
-	{
+	switch(message) {
 	case WM_CREATE:
-		hwndEdit = CreateWindow(TEXT("edit"),
-								nullptr,
-								WS_CHILD | WS_VISIBLE | WS_BORDER | ES_LEFT | ES_MULTILINE | ES_AUTOVSCROLL | ES_NOHIDESEL,
-								0,
-								0,
-								0,
-								0,
-								hwnd,
-								(HMENU)ID_EDIT,
-								((LPCREATESTRUCT)lParam)->hInstance,
-								nullptr);
+		hwndEdit = CreateWindow(
+			TEXT("edit"),
+			nullptr,
+			WS_CHILD | WS_VISIBLE | WS_BORDER | ES_LEFT | ES_MULTILINE |
+			  ES_AUTOVSCROLL | ES_NOHIDESEL,
+			0,
+			0,
+			0,
+			0,
+			hwnd,
+			(HMENU)ID_EDIT,
+			((LPCREATESTRUCT)lParam)->hInstance,
+			nullptr);
 
 		InitTrayIconData(hwnd);
 		Shell_NotifyIcon(NIM_ADD, &nid);
@@ -192,10 +184,13 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
 		return 0;
 
 	case WM_COMMAND:
-		if(LOWORD(wParam) == ID_EDIT)
-		{
+		if(LOWORD(wParam) == ID_EDIT) {
 			if(HIWORD(wParam) == EN_ERRSPACE || HIWORD(wParam) == EN_MAXTEXT)
-				MessageBox(hwnd, TEXT("Edit control out of space."), szAppName, MB_OK | MB_ICONSTOP);
+				MessageBox(
+					hwnd,
+					TEXT("Edit control out of space."),
+					szAppName,
+					MB_OK | MB_ICONSTOP);
 		}
 
 		return 0;
@@ -205,22 +200,28 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
 		return 0;
 
 	case WM_HOTKEY:
-		if(LOWORD(lParam) == hotkey_modifiers && HIWORD(lParam) == hotkey_char)
+		if(
+			LOWORD(lParam) == hotkey_modifiers &&
+			HIWORD(lParam) == hotkey_char) {
 			ToggleWindowState();
+		}
 		return 0;
 
-	case MSG_TRAYICON:	// We're not checking the id because we've only got one icon
-		if(lParam == WM_LBUTTONUP)
+	// We're not checking the id because we've only got one icon
+	case MSG_TRAYICON:
+		if(lParam == WM_LBUTTONUP) {
 			ToggleWindowState();
-		else if(lParam == WM_RBUTTONDOWN)
-		{
+		}
+		else if(lParam == WM_RBUTTONDOWN) {
 			POINT pos = {0};
 			GetCursorPos(&pos);
 			SetForegroundWindow(hwnd);
-			UINT clicked = TrackPopupMenu(hTrayMenu, TPM_RETURNCMD, pos.x, pos.y, 0, hwnd, nullptr);
+			UINT clicked = TrackPopupMenu(
+				hTrayMenu, TPM_RETURNCMD, pos.x, pos.y, 0, hwnd, nullptr);
 			PostMessage(hwnd, WM_NULL, 0, 0);
-			if(clicked == IDTRAY_EXIT_ITEM)
+			if(clicked == IDTRAY_EXIT_ITEM) {
 				SendMessage(hwnd, WM_CLOSE, 0, 0);
+			}
 		}
 
 		return 0;
@@ -243,8 +244,7 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
 	return DefWindowProc(hwnd, message, wParam, lParam);
 }
 
-void InitTrayIconData(HWND hwnd)
-{
+void InitTrayIconData(HWND hwnd) {
 	HINSTANCE hInstance = (HINSTANCE)GetWindowLong(hwnd, GWL_HINSTANCE);
 	memset(&nid, 0, sizeof(NOTIFYICONDATA));
 	nid.cbSize = sizeof(NOTIFYICONDATA);
@@ -253,33 +253,29 @@ void InitTrayIconData(HWND hwnd)
 	nid.uFlags = NIF_ICON | NIF_MESSAGE | NIF_TIP;
 	nid.uCallbackMessage = MSG_TRAYICON;
 	nid.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ICON));
-	//nid.uVersion = NOTIFYICON_VERSION_4;
 	lstrcpy(nid.szTip, TEXT("Data Aggregator in-dev version"));
 }
 
 // For some odd reason, when the window is made visible by clicking on the tray icon,
 // it doesn't capture keyboard input until I click inside the window.
-void ToggleWindowState()
-{
-	if(windowVisible)
-	{
+void ToggleWindowState() {
+	if(windowVisible) {
 		ShowWindow(hwnd, SW_HIDE);
 
-		//	Get the length of text in the control
+		//	get the length of text in the control
 		int length = GetWindowTextLength(hwndEdit);
 
-		//Now get the text in the control
+		// now get the text in the control
 		LPTSTR text = new TCHAR[length + 1];
 		GetWindowText(hwndEdit, text, length + 1);
 
-		//pase and save the text
+		// parse and save the text
 		ParseAndSave(text);
 
-		//empty the control (or not), depending on the settings chosen by the user
+		// empty the control
 		SetWindowText(hwndEdit, TEXT(""));
 	}
-	else
-	{
+	else {
 		ShowWindow(hwnd, SW_SHOW);
 		SetActiveWindow(hwnd);
 		assert(SetFocus(hwndEdit));
@@ -315,29 +311,39 @@ void ParseAndSave(LPTSTR text)
 	// ;.+
 
 	//	If there is no text, return
-	if(lstrlen(text) == 0)
+	if(lstrlen(text) == 0) {
 		return;
+	}
 
 	std::string str(text);
 	std::smatch res;
 
 	// First we get the text to be stored
 	std::regex text_regex(";(.+)");
-	//	If there is no semicolon, then save the text as is (wont work if text has a semicolon)
-	if(!std::regex_search(str, res, text_regex))
-	{
+	//	If there is no semicolon, then save the text as is
+	// (wont work if text has a semicolon)
+	if(!std::regex_search(str, res, text_regex)) {
 		SaveNotes(saveFile, str);
 		return;
 	}
-	std::string to_store = res[1].str();		// res[1] gets the first capture
-	str = std::regex_replace(str, text_regex, std::string(""), std::regex_constants::format_first_only);	// remove the text, leaving options only
+	// res[1] gets the first capture
+	std::string to_store = res[1].str();		
+	str = std::regex_replace(
+		str,
+		text_regex,
+		std::string(""),
+		// remove the text, leaving options only
+		std::regex_constants::format_first_only);
 	
 	//	Now to handle the options
 	std::regex option_regex(":([^:]+\\))|:([^:]+);");
-	while(std::regex_search(str, res, option_regex))
-	{
+	while(std::regex_search(str, res, option_regex)) {
 		std::string option = res[1].str();
-		str = std::regex_replace(str, option_regex, std::string(""), std::regex_constants::format_first_only);
+		str = std::regex_replace(
+			str,
+			option_regex,
+			std::string(""),
+			std::regex_constants::format_first_only);
 		
 		// good luck understanding this later on, no verbose regex support in std::regex
 		// each option must have a param. use 'void' for empty params
@@ -346,8 +352,7 @@ void ParseAndSave(LPTSTR text)
 		std::regex_match(option, match, func_and_param_capture);
 
 		// if the option is 'file' then save the data to the filename given in param
-		if(match[1].str() == std::string("file"))
-		{
+		if(match[1].str() == std::string("file")) {
 			SaveNotes(match[2].str(), to_store);
 		}
 	}
@@ -356,14 +361,12 @@ void ParseAndSave(LPTSTR text)
 	text = nullptr;
 }
 
-void SaveNotes(const std::string& filename, const std::string& text)
-{
+void SaveNotes(const std::string& filename, const std::string& text) {
 	std::ofstream ofs(filename, std::ios::app);
 	ofs << "\n\n" << GetTimeStamp() << '\n' << text;
 }
 
-std::string GetTimeStamp()
-{
+std::string GetTimeStamp() {
 	auto now = std::chrono::system_clock::now();
 	auto rawtime = std::chrono::system_clock::to_time_t(now);
 	auto timeinfo = std::localtime(&rawtime);
