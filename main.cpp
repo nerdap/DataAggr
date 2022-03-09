b810dda42df6613aad5e890fd415a97945f52bb3
Apoorv Upreti <apoorvupreti@gmail.com>
2022-03-09 17:42:51 +0000

    delete unused code

--- main.cpp
+++ main.cpp
@@ -52,33 +52,6 @@ HWND hwndEdit = nullptr;
 HMENU hTrayMenu = nullptr;
 NOTIFYICONDATA nid;
 
-int CDECL MessageBoxPrintf(TCHAR* szCaption, TCHAR* szFormat, ...)
-{
-     TCHAR szBuffer[1024];
-     va_list pArgList;
-
-	// The va_start macro (defined in STDARG.H) is usually equivalent to:
-	// pArgList = (char*)&szFormat + sizeof(szFormat);
-     va_start(pArgList, szFormat);
-
-    // The last argument to wvsprintf points to the arguments
-     _vsntprintf_s(
-		szBuffer,
-		sizeof(szBuffer) / sizeof(TCHAR), 
-        szFormat,
-		pArgList);
-
-    // The va_end macro just zeroes out pArgList for no good reason
-	va_end(pArgList);
-
-     return MessageBox(nullptr, szBuffer, szCaption, 0);
-}
-
-void debugBox(LPCTSTR debugText)
-{
-	MessageBox(nullptr, debugText, TEXT("Debug Text"), MB_OK);
-}
-
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 void InitTrayIconData(HWND);
 void ParseAndSave(LPTSTR);
